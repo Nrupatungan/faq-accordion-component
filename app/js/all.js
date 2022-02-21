3503b21269b99e5a6b3aa6f5fb492590d5bca06c
Dinesh Sake <dineshsake12345@gmail.com>
2022-02-21 12:37:00 +0530

    Repaired all.js

--- app/js/all.js
+++ app/js/all.js
@@ -2,17 +2,26 @@ const accordions = document.querySelectorAll('.accordion')
 const questions = document.querySelectorAll('.question')
 const ones = document.querySelectorAll('.one')
 
+let currentActive = 0
+console.log(accordions.length)
+
 accordions.forEach(accordion => {
     accordion.addEventListener('click', () => {
         currentActive++
         update()
         accordion.classList.toggle('accordion-drop')
         console.log(accordion)
+        // if(currentActive > 1){
+        //     currentActive = 0
+        //     update()
+        // }
+        // console.log(currentActive)
     })
 })
 
 function update(){
     accordions.forEach(accordion => {
+        currentActive--
         accordion.classList.remove('accordion-drop')
     })
 }
